d7b853ef3c2e7037edf339a9466adb38b96f8d8a
lacko186 <laszlobogdan0619@gmail.com>
2025-07-08 09:24:59 +0200

    végpont teszt node server.js + kommentek

--- server.js
+++ server.js
@@ -5,12 +5,17 @@ const ejs = require('ejs');
 const app = express();
 const port = 5000;
 
+// Middleware
 app.use(cors());
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-
+//main root ami az indexre mutat
 app.get('/', (req, res) => {
     res.render('index', { title: 'Car Shop Server' });
 });
 
+//végpont futtatás
+app.listen(port, () => {
+    console.log(`Szerver fut a http://localhost:${port} címen`);
+});
