3b9b2e5d0603c071359246ed170398494d15c368
lacko186 <laszlobogdan0619@gmail.com>
2025-07-08 09:27:42 +0200

    Schema és adatok megadása

--- server.js
+++ server.js
@@ -15,6 +15,47 @@ app.get('/', (req, res) => {
     res.render('index', { title: 'Car Shop Server' });
 });
 
+const Scema = {
+    id: Number,
+    brand: String,
+    model: String,
+    year: Number,
+    price: Number
+};
+
+let Products = 
+[
+    {
+        id: 1,
+        brand: 'Toyota',
+        model: 'Corolla',
+        year: 2020,
+        price: 20000
+    },
+    {
+        id: 2,
+        brand: 'Honda',
+        model: 'Civic',
+        year: 2019,
+        price: 22000
+    },
+    {
+        id: 3,
+        brand: 'Ford',
+        model: 'Focus',
+        year: 2021,
+        price: 25000
+    },
+    {
+        id: 4,
+        brand: 'Chevrolet',
+        model: 'Malibu',
+        year: 2018,
+        price: 18000
+    }
+];
+
+
 //végpont futtatás
 app.listen(port, () => {
     console.log(`Szerver fut a http://localhost:${port} címen`);
