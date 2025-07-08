f0a859dd2799184864f0ecb8aa7a7e822a9cfa5e
lacko186 <laszlobogdan0619@gmail.com>
2025-07-08 09:18:29 +0200

    ejs alap html, szerver üzenet, végpont nevének megadása, stílus

--- server.js
+++ server.js
@@ -4,3 +4,9 @@ const cors = require('cors');
 const ejs = require('ejs');
 const app = express();
 const port = 5000;
+
+app.use(cors());
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+
