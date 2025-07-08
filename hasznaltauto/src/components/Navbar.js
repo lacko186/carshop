15c4c5c03a73dba90c85abe33e951445af4a5f9c
lacko186 <laszlobogdan0619@gmail.com>
2025-07-08 10:11:28 +0200

    Navbar componens

---
+++ hasznaltauto/src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+import 'react-bootstrap/dist/react-bootstrap.min.js';
+
+const Navbar = () => {
+    return (
+ <div>
+    <h1>CarShop</h1>
+ </div>
+    );
+}
+export default Navbar;
